a0af0fe960e89eeaeff123169c4ef2c6568e9c60
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2023-05-23 22:22:26 -0300

    Create tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,10 @@
+//Fazendo a tabuada do número desejado
+
+function tabuada (num) { 
+    for (i = 1; i <= 10; i++) {
+        console.log (num, "x", i, "=", num*i)
+    }
+}
+
+tabuada (2)
+tabuada (5)
