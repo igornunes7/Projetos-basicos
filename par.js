0ad2f6ef63ffd0e7b00a7c01de09b5ca74b9b870
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2023-05-23 22:16:43 -0300

    Create par.js

---
+++ par.js
@@ -0,0 +1,11 @@
+function is_par (num) {
+    if (num % 2 === 0) {
+        console.log(num, "é par")
+    } else {
+        console.log(num, "é impar")
+    }
+}
+
+is_par (11)
+is_par (12)
+is_par (81)
